de31044e5571c367355b70e70c733edb8d5a4b46
Bart Huiskamp <bartolameus.h@gmail.com>
2024-02-10 15:49:40 +0100

    3 type of side dice added

--- src/Controller/RollDiceController.php
+++ src/Controller/RollDiceController.php
@@ -9,7 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class RollDiceController extends AbstractController
 {
     #[Route('/D6', name: 'roll_dice_D6')]
-    public function index(): Response
+    public function rollD6(): Response
+    {
+        $D6 = rand(1, 6);
+        return $this->render('roll_dice/index.html.twig', [
+            'controller_name' => '6 SideDice',
+            'dice' => $D6,
+        ]);
+    }
+    
+    #[Route('/D8', name: 'roll_dice_D8')]
+    public function rollD8(): Response
+    {
+        $D8 = rand(1, 8);
+        return $this->render('roll_dice/index.html.twig', [
+            'controller_name' => '8 SideDice',
+            'dice' => $D8,
+        ]);
+    }
+    
+    #[Route('/D20', name: 'roll_dice_D20')]
+    public function rollD20(): Response
+    {
+        $D20 = rand(1, 20);
+        return $this->render('roll_dice/index.html.twig', [
+            'controller_name' => '20 SideDice',
+            'dice' => $D20,
+        ]);
+    }
+    public function index2(): Response
     {
         // $diceNumber = rand(1,6);
         function rollDice(){
