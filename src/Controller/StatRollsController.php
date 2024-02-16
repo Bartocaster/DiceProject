904184873882f9c6484e8da971ddd428d61a77d3
Bart Huiskamp <bartolameus.h@gmail.com>
2024-02-16 12:42:26 +0100

    Copied and modified the function for dice for the state roll

--- src/Controller/StatRollsController.php
+++ src/Controller/StatRollsController.php
@@ -3,11 +3,62 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\VarDumper\VarDumper;
+use App\Controller\RollDiceController;
 
 class StatRollsController extends AbstractController
 {
+    private $rollDiceController;
+
+    public function __construct(RollDiceController $rollDiceController)
+    {
+        $this->rollDiceController = $rollDiceController;
+    }
+
+    #[Route('/stat', name: 'stat_roll')]
+    public function statRoll(Request $request): Response
+    {
+        // $amountOfDice = $request->query->getInt('amountOfDice', 1);
+        $countsJson = $request->query->get('counts');
+        $dNumDice = json_decode($countsJson, true);
+        $diceResults = [];
+        $total = 0; 
+        
+        $ranNumDiceGen = $this->rollDiceController->randNumDiceGenerator($dNumDice);
+        
+        $diceResults = $ranNumDiceGen['diceResults'];
+        $total = $ranNumDiceGen['total']; 
+        $individualRolls = $ranNumDiceGen['individualRolls'];
+
+        $noDice = "No dice. You gotte choose them.";
+        $dice = implode(' + ', $diceResults); // Combine the dice results into a string
+        $combine = $individualRolls . ' total = ' . $total;
+        if (count($diceResults) === 1 ) {
+            return new JsonResponse([
+                'dice' => $individualRolls,
+                'choseDice' => $dNumDice,
+                // 'typeOfDice' => $individualRolls,
+            ]);
+        } elseif (count($diceResults) === 0 ) {
+            return new JsonResponse([
+                'dice' => $noDice,
+            ]);
+        } else {   
+            return new JsonResponse([
+                'dice' => $combine,
+                // 'results' => $diceResults,
+                // 'choseDice' => $dNumDice,
+                'typeOfDice' => $individualRolls,
+            ]);
+        }
+
+    }
+   
+
     #[Route('/statRolls', name: 'stat_rolls')]
     public function index(): Response
     {
